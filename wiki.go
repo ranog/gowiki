59a6b94adf78c71cecc1d8952339fad0ef37f1d1
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-03-21 21:34:43 -0300

    add main method

--- wiki.go
+++ wiki.go
@@ -1,6 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type Page struct {
 	Title string
@@ -19,4 +22,11 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
